4b9adfc30b2c86f224a91be4ec9a98273c883c1c
BarszczewskiKonrad <konradbarszczew@gmail.com>
2026-03-08 13:54:49 +0100

    Update plots.py

--- code/plots.py
+++ code/plots.py
@@ -1,9 +1,32 @@
+"""
+Symulacja Monte Carlo prawdopodobieństwa bankructwa portfela ubezpieczeń na życie.
+
+Model analizuje jak na prawdopobieństwo bankructwa wpływa:
+    - wielkość portfela
+    - forma narzutu finansowego
+    - wielkość narzutu finansowego
+
+Autor: Konrad Barszczewski
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.pyplot import xscale
 
 
 def plot_paths(paths,show = True,save_path = None):
+    """
+    Wykres przebiegów stanu portfela, 5. oraz 95. centyla i mediany z wyników
+    Oś X: kolejne lata trwania umowy
+    Oś Y: Stan portfela w danym roku
+    Parametry:
+    ----------
+    paths : list
+        Tablica przebiegów stanu portfela z symulacji Monte Carlo
+    show : bool
+        Czy wykres ma być pokazany?
+    save_path: str
+        Ścieżka zapisu pliku
+    """
     plt.style.use("seaborn-v0_8")
     paths = np.array(paths)
     for path in paths:
@@ -31,6 +54,19 @@ def plot_paths(paths,show = True,save_path = None):
         plt.show()
 
 def plot_histogram(finals,show = True,save_path = None):
+        """
+    Histogram stanów końcowych portfela
+    Oś X: Stan końcowy portfela
+    Oś Y: Ilość symulacji
+    Parametry:
+    ----------
+    finals : list
+        Lista stanów końcowych portfela z symulacji Monte Carlo
+    show : bool
+        Czy wykres ma być pokazany?
+    save_path: str
+        Ścieżka zapisu pliku
+    """
     plt.hist(finals, bins = 30 )
 
     plt.xlabel("Kapitał końcowy")
@@ -45,6 +81,25 @@ def plot_histogram(finals,show = True,save_path = None):
 
 
 def plot_bank_prob(propabilities,arguments,aplha_or_n,show = True,save_path = None):
+    """
+    Wykres szansy oszacowanej szansy bankructwa danego portfela z symulacji Monte Carlo w zależności od liczby klientów lub współczynnika narzutu alpha
+    Oś X: Liczba klientów (w skali logarytmicznej) lub parametr alpha
+    Oś Y: Prawdopodobieństwo bankructwa
+
+    Parametry:
+    -----------
+    propabilities : list
+        Lista oszacowanych prawdopodobieństw bankructwa
+    arguments : list
+        Lista ilości klientów n lub parametrów alpha
+    alpha_or_n : str
+        Parametr względem którego badaliśmy prawdopodobieństwo
+    show : bool
+        Czy wykres ma być pokazany?
+    save_path: str
+        Ścieżka zapisu pliku
+        
+    """
     plt.style.use("seaborn-v0_8")
     if aplha_or_n == "aplha":
         pass
